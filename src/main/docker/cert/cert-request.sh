22a30d4c2f7354ab3dbb655dc3fa925544bfc0df
Levente Farkas <thebe14@gmail.com>
2023-04-11 20:49:16 +0200

    CORS now configurable from .env file
    Turned off HTTPS access log for the Java app
    Fixed SSL terminator not using issued Let's Encrypt cert

--- src/main/docker/cert/cert-request.sh
+++ src/main/docker/cert/cert-request.sh
@@ -3,7 +3,7 @@
 if [ ! -f /var/www/html ]; then
     mkdir -p /var/www/html
 fi
- 
+
 if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
 	certbot certonly \
 			--config-dir ${LETSENCRYPT_DIR:-/etc/letsencrypt} \
@@ -22,8 +22,8 @@ if [ -n "$DOMAIN" ] && [ "$DOMAIN" != "localhost" ]; then
 		chmod +rx ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive
 		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/fullchain*.pem
 		chmod +r  ${LETSENCRYPT_DIR:-/etc/letsencrypt}/archive/${DOMAIN}/privkey*.pem
-		cp -rf ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem /usr/share/nginx/certificates/privkey.pem
-		cp -rf ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem /usr/share/nginx/certificates/fullchain.pem
+		cp -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem /usr/share/nginx/certificates/privkey.pem
+		cp -f ${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem /usr/share/nginx/certificates/fullchain.pem
 		echo "Copied new certificate to /usr/share/nginx/certificates"
 	fi
 fi

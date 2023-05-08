2a3807545e11238627300a49fc2f2688ae5695ff
Levente Farkas <thebe14@gmail.com>
2023-05-08 18:11:06 +0200

    Drop commented out lines

--- src/main/java/eosc/eu/MetricsCustomization.java
+++ src/main/java/eosc/eu/MetricsCustomization.java
@@ -67,7 +67,6 @@ public class MetricsCustomization {
                     }
 
                     return builder
-                            //.percentilesHistogram(true)
                             .build()
                             .merge(config);
                 }

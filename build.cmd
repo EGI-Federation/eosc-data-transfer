f82f41d8d76d7b26b3bb6a99581d102e5494fd32
Levente Farkas <thebe14@gmail.com>
2023-10-27 16:28:08 +0200

    Removed root scripts

--- build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo Building the EOSC Data Transfer Proxy...
-
-cd src\main\docker
-docker-compose -p eosc-beyond up -d --build --remove-orphans
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Building the EOSC Data Transfer Proxy..."
-
-cd src/main/docker
-sudo docker-compose -p eosc-beyond up -d --build --remove-orphans
